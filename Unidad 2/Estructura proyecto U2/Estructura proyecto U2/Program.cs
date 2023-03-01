e4a4c55596c6528e754c1061b44b37652786ce48
up210368 <up210368@alumnos.upa.edu.mx>
2023-03-01 01:59:12 -0600

    Proyecto U2

--- Unidad 2/Estructura proyecto U2/Estructura proyecto U2/Program.cs
+++ Unidad 2/Estructura proyecto U2/Estructura proyecto U2/Program.cs
@@ -6,30 +6,113 @@ namespace proyecto
 {
     class selectionmethod
     {
-        static void Main()
+        static int origRow;
+        static int origCol;
+        static int[] array = new int[20];
+        // - - - - - - - Cargar numeros - - - - - - - - 
+        // Lee un .txt con numeros aleatorios para crear un array e imprimirlo
+        static void CargarNum(int[] array)
         {
-            int numero = 99;
-            int x = 2;
-            int y = 1;
-            barra(numero, x, y);
-            Console.ReadLine();
-            barra(25, x, y+2);
-            Console.ReadLine();
-            barra(36, x, y+4);
-            Console.ReadLine();
-            barra(47, x, y+6);
-            Console.ReadLine();
-            barra(79, x, y+8);
-            Console.ReadLine();
-            barra(69, x, y+8);
-            Console.SetCursorPosition(0, 25);
+            string num;
+            try
+            {
+                //StreamReader sr = new StreamReader("C:\\Users\\Jona_\\OneDrive\\Escritorio\\numeros.txt");
+                StreamReader sr = new StreamReader("C:\\Users\\isai4470\\Desktop\\programacion\\Visual Studio Community\\Estructura-de-datos\\Unidad 2\\numeros.txt");
+                num = sr.ReadLine();
+                int i = 0;
+                while (num != null)
+                {
+                    array[i] = Convert.ToInt32(num);
+                    num = sr.ReadLine();
+                    i++;
+                }
+                sr.Close();
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("No se pudo abrir el archivo");
+
+            }
+
         }
-        static void barra(int numero, int x, int y)
+
+        // - - - - - - - M A I N - - - - - - - - 
+        static void Main(string[] args)
+        {
+            //Console.OutputEncoding = System.Text.Encoding.Unicode;
+            //int[] array = { 10, 5, 2, 8, 7, 6, 10, 2, 4, 8, 59, 69, 70, 83, 96, 29, 19, 34, 62, 44 };
+            CargarNum(array);
+            Console.Clear();
+            valores();
+            origRow = Console.CursorTop;
+            origCol = Console.CursorLeft;
+            Impresion(array);
+            Console.SetCursorPosition(0, 41);
+            Console.Read();
+            seleccion(array);
+            Console.SetCursorPosition(0, 44);
+            Console.WriteLine("fin."); Console.ReadLine();
+        }
+
+        // - - - - - - - - I M P R E S I O N - - - - - - - -
+        static void Impresion(int[] array)
+        {
+            for (int i = 2; i <= 40; i = i + 2)
+            {
+                barra(array[(i - 2) / 2], i);
+            }
+        }
+
+        // - - - - - - - S E L E C T I O N - - - - - - - - - - 
+        // En esta funcion se ordenan los números y llama a las barras
+        static void seleccion(int[] array)
+        {
+            int menor = 0;
+            int pos = 0;
+            int aux = 0;
+            int contador = 0;
+            int pasos = 0;
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                menor = array[i];
+                pos = i;
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[j] < menor)
+                    {
+                        menor = array[j];
+                        pos = j;
+                    }
+                    contador++;
+                }
+                if (pos != i)
+                {
+                    aux = array[i];
+                    array[i] = array[pos];
+                    array[pos] = aux;
+                }
+                pasos++;
+                barra(array[i], i * 2 + 2);
+                Console.SetCursorPosition(0, 41); Console.Read();
+                barra(array[pos], pos * 2 + 2);
+                Console.SetCursorPosition(0, 41); Console.Read();
+
+            }
+            Console.WriteLine("Se hicieron " + pasos + " pasadas");
+            Console.WriteLine("Se hicieron " + contador + " comparaciones");
+        }
+
+        // - - - - - - - - B A R R A S - - - - - - - - - -
+        // en esta funcion se representa el numero deseado en barras
+        static void barra(int numero, int y)
         {
             int[] valor = new int[4] { 20, 10, 5, 1 };
             char cuadro = '█';
             int i = 0;
-            borrar(x, y); // borra la barra en esa posicion
+            int x = 1;
+            int num = numero;
+            borrar(y); // borra la barra en esa posicion
             do
             {
                 if (numero >= valor[i])
@@ -59,32 +142,38 @@ namespace proyecto
                 i++;
             } while (numero != 0);
             Console.ResetColor();
+            Console.Write("   " + num);
         }
-        static void borrar(int x,int y)
+        static void borrar(int pos)
         {
-            for (int i = 0; i < 10; i++)
+            Console.SetCursorPosition(1, pos);
+            Console.Write("                                    ");
+        }
+        static void valores()
+        {
+            char cuadro = '█';
+            int x = 1;
+            for (int j = 0; j < 4; j++)
             {
-                Console.SetCursorPosition(x, y);
-                Console.Write(' ');
-                x += 2;
+                Console.SetCursorPosition(x, 0);
+                switch (j)
+                {
+                    case 0:
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        Console.Write(cuadro + " = 20"); break;
+                    case 1:
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+                        Console.Write(cuadro + " = 10"); break;
+                    case 2:
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write(cuadro + " = 5"); break;
+                    case 3:
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.Write(cuadro + " = 1"); break;
+                }
+                x += 7;
             }
+            Console.ResetColor();
         }
     }
 }
-
-
-/* programa que use el metodo de seleccion
-            int count = 0;
-            int[] array = { 3, 10, 6, 9, 8, 1, 7, 5, 4, 2};
-            for (int i=0; i<9; i++)
-                for (int j = 0; j < 9-i; j++)
-                {
-                    if (array[j] <= array[j + 1])
-                    {
-                        int temp = array[j];
-                        array[j] = array[j + 1];
-                        array[j + 1] = temp;
-                    }
-                    count++;
-                }
-            Console.ReadLine();*/
